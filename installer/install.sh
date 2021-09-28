1a891c2fa72f9ac620236606eddd909f21a4a94a
Colin Sauze <cos@aber.ac.uk>
2021-09-28 14:33:23 +0100

    setting hostname to carpenpi

--- installer/install.sh
+++ installer/install.sh
@@ -4,9 +4,16 @@
 sudo apt update -y
 sudo apt upgrade -y
 
+#set the hostname to carpenpi
+sudo hostname carpenpi
+echo carpenpi | sudo tee /etc/hostname
+sudo sed -i 's/raspberrypi/carpenpi/g' /etc/hosts
+
 sudo ./setup_gitea.sh
 sudo ./apache2.sh
 sudo ./setup_jekyll.sh
 ./build_webpages.sh
 sudo ./deploy_webpages.sh
 sudo ./setup_cran.sh
+
+sudo reboot
