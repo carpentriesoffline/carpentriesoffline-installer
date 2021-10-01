c574dae02a2e778b52dcfd396486290ef3f292cf
Colin Sauze <cos@aber.ac.uk>
2021-10-01 11:00:12 +0100

    moving the list of lessons to download to its own file

--- installer/build_webpages.sh
+++ installer/build_webpages.sh
@@ -6,9 +6,9 @@
 mkdir sites
 cd sites
 
-#list of swcarpentry repositories at https://github.com/swcarpentry/swcarpentry
 #loop through all the repositories we want to copy and build their webpages
-for repo in "https://github.com/swcarpentry/shell-novice" "https://github.com/swcarpentry/git-novice" "https://github.com/swcarpentry/r-novice-inflammation"; do
+#the file lessons contains a list of every lesson we'll build
+for repo in $(cat ../lessons) ; do
     repo_name=$(basename $repo)
     git clone $repo
     cd $repo_name
