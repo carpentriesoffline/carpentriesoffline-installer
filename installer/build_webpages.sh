c2e2dcb9ca5b4c7c9124988a5786e134049691bb
Colin Sauze <cos@aber.ac.uk>
2021-10-01 16:36:36 +0100

    seperating the build of jekyll and rmarkdown lessons

--- installer/build_webpages.sh
+++ installer/build_webpages.sh
@@ -8,13 +8,22 @@ cd sites
 
 #loop through all the repositories we want to copy and build their webpages
 #the file lessons contains a list of every lesson we'll build
-for repo in $(cat ../lessons) ; do
+for repo in $(cat ../lessons.jekyll) ; do
     repo_name=$(basename $repo)
     git clone $repo
     cd $repo_name
     #we aren't using jekyll from Gem and the Gemfile being present breaks the system version of jekyll
     rm Gemfile
     jekyll build
+    cd ..
+done
+
+
+for repo in $(cat ../lessons.rmarkdown) ; do
+    repo_name=$(basename $repo)
+    git clone $repo
+    cd $repo_name
+    sudo make
     cd ..
 done
 
--- installer/setup_jekyll.sh
+++ installer/setup_jekyll.sh
@@ -1,5 +1,17 @@
-apt-get install -y git jekyll build-essential
+apt install -y git jekyll build-essential
 
 #needed to run bundler, but not if we use jekyll from apt
 #apt-get install -y ruby ruby-dev libxml2-dev ruby-bundler
+
+#needed to build r markdown data carpentry lessons
+apt install -y libcurl4-openssl-dev libxml2-dev r-cran-remotes r-cran-curl 
+
+#are these doing anything? R seems to ignore them
+#r-cran-broom r-cran-cachem r-cran-callr r-cran-cellrange r-cran-dtplyr r-cran-ellipsis r-cran-farver r-cran-fastmap r-cran-forcats r-cran-fs r-cran-gargle \
+#r-cran-ggplot2 r-cran-googledrive r-cran-googlesheets4 r-cran-gridExtra r-cran-gtable r-cran-haven r-cran-hexbin r-cran-httr r-cran-hunspell r-cran-ids r-cran-isonband r-cran-jquerylib \
+#r-cran-labeling r-cran-lifrecycle r-cran-lubridate r-cran-magrittr r-cran-memoise r-cran-mime r-cran-modelr  r-cran-munsell r-cran-openssl  r-cran-patchwork r-cran-pillar r-cran-pkgconfig \
+#r-cran-plogr r-cran-prettyunits r-cran-processx r-cran-progress r-cran-ps r-cran-purrr r-cran-R6  r-cran-rappdirs r-cran-RColorBrewer r-cran-Rcpp r-cran-readr r-cran-readxl r-cran-rematch \
+#r-cran-rematch2 r-cran-remotes r-cran-reprex r-cran-rlang r-cran-rmarkdown r-cran-RSQLite r-cran-rstudioapi r-cran-rvest r-cran-scales r-cran-selectr r-cran-stringi r-cran-sys r-cran-tibble \
+#r-cran-tidyr r-cran-tidyselect r-cran-tidyverse r-cran-tinytex r-cran-tzdb r-cran-utf8 r-cran-uuid r-cran-vctrs r-cran-viridisLite r-cran-vroom  r-cran-withr r-cran-xfun r-cran-xml2 r-cran-yaml
+
 
