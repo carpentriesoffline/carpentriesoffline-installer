ea98808b8a831d52b968390361515424b8b04ef9
Colin Sauze <cos@aber.ac.uk>
2021-10-01 12:11:46 +0100

    Revert "files to turn the Pi into an access point"
    This reverts commit 3d66a24a676078cb1d8736c834685ab53a52d225.

--- installer/setup_accesspoint.sh
+++ installer/setup_accesspoint.sh
@@ -3,16 +3,16 @@
 # script to install software/config to setup an access point on the Pi
 
 # install packages, hostap for running the access point, isc-dhcp-server to provide IP addresses and NTP to provide time configuration
-apt -y install hostapd dnsmasq ntp
+apt -y install hostapd isc-dhcp-server ntp
 
 # copy config files
-cp access_point_files/interfaces /etc/network/
-cp access_point_files/iptables.ipv4.nat /etc
-cp access_point_files/hostapd.conf /etc/hostapd
-cp access_point_files/hostapd /etc/default/
-cp access_point_files/dnsmasq /etc/default/
-cp access_point_files/dnsmasq.conf /etc/
-cp access_point_files/ntp.conf /etc/ntp.conf
+cp interfaces /etc/network/
+cp iptables.ipv4.nat /etc
+cp hostapd.conf /etc/hostapd
+cp hostapd /etc/default/hostapd
+cp isc-dhcp-server /etc/default/isc-dhcp-server
+cp dhcpd.conf /etc/dhcp/dhcpd.conf
+cp ntp.conf /etc/ntp.conf
 
 # allow forwarding/internet access via the Pi
 echo "net.ipv4.ip_forward=1" >> /etc/sysctl.conf
