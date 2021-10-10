3e10acc09f9331ad1161406f6f9772e04ca85f59
Jamie Quinn <jamiejquinn@jamiejquinn.com>
2021-10-10 12:16:11 +0100

    Output qemu log

--- test/start_qemu.sh
+++ test/start_qemu.sh
@@ -14,17 +14,17 @@ ls
 export QEMU_AUDIO_DRV=none # disable audio
 
 qemu-system-arm \
- -M versatilepb \
- -cpu arm1176 \
- -m 256 \
- -drive "file="$PI_IMG",if=none,index=0,media=disk,format=raw,id=disk0" \
- -device "virtio-blk-pci,drive=disk0,disable-modern=on,disable-legacy=off" \
- -net "user,hostfwd=tcp::5022-:22" \
- -net nic \
- -dtb qemu-rpi-kernel/versatile-pb-buster-5.4.51.dtb \
- -kernel qemu-rpi-kernel/kernel-qemu-5.4.51-buster \
- -append 'root=/dev/vda2 panic=1' \
- -no-reboot \
- -display none
- #-display none \
- #-daemonize
+  -D /tmp/qemu.log \
+  -M versatilepb \
+  -cpu arm1176 \
+  -m 256 \
+  -drive "file="$PI_IMG",if=none,index=0,media=disk,format=raw,id=disk0" \
+  -device "virtio-blk-pci,drive=disk0,disable-modern=on,disable-legacy=off" \
+  -net "user,hostfwd=tcp::5022-:22" \
+  -net nic \
+  -dtb qemu-rpi-kernel/versatile-pb-buster-5.4.51.dtb \
+  -kernel qemu-rpi-kernel/kernel-qemu-5.4.51-buster \
+  -append 'root=/dev/vda2 panic=1' \
+  -no-reboot \
+  -display none \
+  -daemonize
