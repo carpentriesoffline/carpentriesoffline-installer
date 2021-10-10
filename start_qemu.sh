f28ffe6b8a9db25bbfd95783672e5e3b249acd79
Jamie Quinn <jamiejquinn@jamiejquinn.com>
2021-10-10 09:58:24 +0100

    When running qemu, use passed in image name and ls hoem dir

--- start_qemu.sh
+++ start_qemu.sh
@@ -1,14 +1,18 @@
 #!/usr/bin/env bash
 
+PI_IMG=$1
+echo $PI_IMG
 echo $USER
 echo $HOME
 cd $HOME
+
+ls -l
 
 qemu-system-arm \
  -M versatilepb \
  -cpu arm1176 \
  -m 256 \
- -drive "file=2021-01-11-raspios-buster-armhf-lite.img,if=none,index=0,media=disk,format=raw,id=disk0" \
+ -drive "file="$PI_IMG",if=none,index=0,media=disk,format=raw,id=disk0" \
  -device "virtio-blk-pci,drive=disk0,disable-modern=on,disable-legacy=off" \
  -net "user,hostfwd=tcp::5022-:22" \
  -net nic \
