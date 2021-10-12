fa1621040b6c35cbf71b3dc4b2bc89815d8847c8
Colin Sauze <cos@aber.ac.uk>
2021-10-12 23:32:53 +0100

    fixing path to content download script

--- installer/setup_content.sh
+++ installer/setup_content.sh
@@ -5,7 +5,4 @@ apt install python3-pip
 pip3 install git+https://git@github.com/CarpenPi/web-free-data-science.git
 
 cd /var/www/html/
-~/.local/bin/carpenpi download .
-
-
-
+/usr/local/bin/carpenpi download .
