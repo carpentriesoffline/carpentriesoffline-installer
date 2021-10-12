9aed4ef16070397ff5f35de1240eb8e7884236cc
Colin Sauze <cos@aber.ac.uk>
2021-10-12 23:41:17 +0100

    hack to fix R download URLs for now

--- installer/setup_content.sh
+++ installer/setup_content.sh
@@ -4,5 +4,8 @@ apt install python3-pip
 
 pip3 install git+https://git@github.com/CarpenPi/web-free-data-science.git
 
+#hack to fix R version until pull request fixing this upstream is accepted
+sed -i 's/R-4.1.0/R-4.1.1/g' /usr/local/lib/python3.7/dist-packages/carpenpi/urls.py
+
 cd /var/www/html/
 /usr/local/bin/carpenpi download .
