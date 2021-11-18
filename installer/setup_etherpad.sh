99d593e06dd468038ceffd189db5f2d7aaccdc2f
Colin Sauze <cos@aber.ac.uk>
2021-11-18 18:16:57 +0000

    scripts to setup etherpad as a service

--- installer/setup_etherpad.sh
+++ installer/setup_etherpad.sh
@@ -3,8 +3,25 @@
 #this installs extra apt sources, does it have the right arm binaries?
 curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
 sudo apt install -y nodejs
-git clone --branch master https://github.com/ether/etherpad-lite.git
-cd etherpad-lite/src/bin
-export NODE_ENV=production
-./run.sh
+
+
+#create a jupyter user
+sudo adduser \
+   --system \
+   --shell /bin/bash \
+   --gecos 'Etherpad' \
+   --group \
+   --disabled-password \
+   --home /home/etherpad \
+   etherpad
+
+cd /home/etherpad
+sudo -s -u etherpad git clone --branch master https://github.com/ether/etherpad-lite.git
+sudo -s -u etherpad sed -i 's/"suppressErrorsInPadText": false/"suppressErrorsInPadText": true/' /home/etherpad/etherpad-lite/settings.json
+
 cd -
+sudo cp etherpad.service /etc/systemd/system/
+sudo systemctl enable etherpad
+sudo systemctl start etherpad
+
+
