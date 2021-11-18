664761e81eb98f7cd09e3232e4459da06915a496
Colin Sauze <cos@aber.ac.uk>
2021-11-18 17:57:12 +0000

    fix for latest version of R

--- installer/setup_content.sh
+++ installer/setup_content.sh
@@ -5,7 +5,7 @@ apt install python3-pip
 pip3 install git+https://git@github.com/CarpenPi/web-free-data-science.git
 
 #hack to fix R version until pull request fixing this upstream is accepted
-sed -i 's/R-4.1.0/R-4.1.1/g' /usr/local/lib/python3.7/dist-packages/carpenpi/urls.py
+sed -i 's/R-4.1.1/R-4.1.2/g' /usr/local/lib/python3.7/dist-packages/carpenpi/urls.py
 
 cd /var/www/html/
 /usr/local/bin/carpenpi download .
