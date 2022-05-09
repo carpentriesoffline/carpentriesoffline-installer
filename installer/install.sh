fe2c446206cf6dc8da3083208bae428b5ba00296
Colin Sauze <cos@aber.ac.uk>
2022-05-10 00:22:25 +0100

    changing hostname and cleaning up which setup scripts are run

--- installer/install.sh
+++ installer/install.sh
@@ -7,18 +7,15 @@ sudo apt upgrade -y
 #install key utilities just in case we don't have them
 sudo apt install -y wget unzip git build-essential
 
-#set the hostname to carpenpi
-sudo hostname carpenpi
-echo carpenpi | sudo tee /etc/hostname
-sudo sed -i 's/raspberrypi/carpenpi/g' /etc/hosts
-echo "192.168.1.1     carpenpi.org www.carpenpi.org" | sudo tee -a /etc/hosts
+#set the hostname to carpentries-offline
+sudo hostname carpentries-offline
+echo carpentries-offline | sudo tee /etc/hostname
+sudo sed -i 's/raspberrypi/carpentries-offline/g' /etc/hosts
+echo "192.168.1.1     carpentries-offline.org www.carpentries-offline.org" | sudo tee -a /etc/hosts
 
 sudo ./setup_rpi.sh
 sudo ./apache2.sh
 sudo ./setup_content.sh
-#./build_webpages.sh
-#sudo ./deploy_webpages.sh
-sudo ./setup_cran.sh
 sudo ./setup_gitea.sh
 sudo ./setup_accesspoint.sh
 
