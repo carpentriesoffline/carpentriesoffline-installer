4839304c0063f3426fc3513cb25916b7ce79b9d0
Colin Sauze <cos@aber.ac.uk>
2022-05-19 13:03:38 +0000

    clearing screen between each stage as qemu is getting into a state where we can only see one line

--- installer/install.sh
+++ installer/install.sh
@@ -13,10 +13,25 @@ echo carpentries-offline | sudo tee /etc/hostname
 sudo sed -i 's/raspberrypi/carpentries-offline/g' /etc/hosts
 echo "192.168.1.1     carpentries-offline.org www.carpentries-offline.org" | sudo tee -a /etc/hosts
 
+echo "Setting up Raspberry Pi"
 sudo ./setup_rpi.sh
-sudo ./apache2.sh
-sudo ./setup_content.sh
-sudo ./setup_gitea.sh
-sudo ./setup_accesspoint.sh
+clear
 
+echo "Setting up Web Server"
+sudo ./apache2.sh
+clear
+
+echo "Getting Content"
+sudo ./setup_content.sh
+clear
+
+echo "Setting up Gitea"
+sudo ./setup_gitea.sh
+clear
+
+echo "Setting up Access Point"
+sudo ./setup_accesspoint.sh
+clear
+
+echo "Done, rebooting"
 sudo reboot
