c2bd6c6edb04ad80c25104539e689d9f0cbe5e64
Colin Sauze <cos@aber.ac.uk>
2022-05-19 13:03:20 +0000

    allowing script to work with any image

--- test/run_qemu.sh
+++ test/run_qemu.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-qemu-system-aarch64 -m 1024 -M raspi3b -kernel kernel8.img -dtb bcm2710-rpi-3-b-plus.dtb -sd /build_img/2021-01-11-raspios-buster-armhf-lite.img -append "console=ttyAMA0 root=/dev/mmcblk0p2 rw rootwait rootfstype=ext4" -nographic -device usb-net,netdev=net0 -netdev user,id=net0,hostfwd=tcp::5555-:22 -no-reboot
+qemu-system-aarch64 -m 1024 -M raspi3b -kernel kernel8.img -dtb bcm2710-rpi-3-b-plus.dtb -sd $1 -append "console=ttyAMA0 root=/dev/mmcblk0p2 rw rootwait rootfstype=ext4" -nographic -device usb-net,netdev=net0 -netdev user,id=net0,hostfwd=tcp::5555-:22 -no-reboot
 
