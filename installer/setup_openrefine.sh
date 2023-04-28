2ebf06708f54ebf0bf65887b3d4a62a056b6fe89
Colin Sauze <colsau@noc.ac.uk>
2023-04-28 13:26:54 +0100

    creating a user for openrefine too

--- installer/setup_openrefine.sh
+++ installer/setup_openrefine.sh
@@ -3,11 +3,23 @@
 #install java runtime environment
 apt-get -y install default-jre-headless
 
+#create an openrefine user
+sudo adduser \
+   --system \
+   --shell /bin/bash \
+   --gecos 'Openrefine' \
+   --group \
+   --disabled-password \
+   --home /home/openrefine \
+   openrefine
+
+cd /home/openrefine
+
 #install openrefine
 wget https://github.com/OpenRefine/OpenRefine/releases/download/3.7.2/openrefine-linux-3.7.2.tar.gz
 tar xvfz openrefine-linux-3.7.2.tar.gz
-mv openrefine-3.7.2 /usr/local/
 
+cd -
 #enable openrefine service
 cp openrefine.service /etc/systemd/system/
 systemctl enable openrefine
