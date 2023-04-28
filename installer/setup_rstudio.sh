b532ba2d87841f95bf49798948e94befe395921b
Colin Sauze <colsau@noc.ac.uk>
2023-04-29 00:47:45 +0100

    Create setup_rstudio.sh

---
+++ installer/setup_rstudio.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#install java runtime environment
+apt-get -y install r-base
+
+#create an openrefine user
+sudo adduser \
+   --system \
+   --shell /bin/bash \
+   --gecos 'rstudio' \
+   --group \
+   --disabled-password \
+   --home /home/rstudio \
+   rstudio
+
+cd /home/rstudio
+
+#install openrefine
+wget wget https://download2.rstudio.org/server/bionic/amd64/rstudio-server-2023.03.0-386-amd64.deb
+dpkg -i rstudio-server-2023.03.0-386-amd64.deb
+
+cd -
+#enable rstudio service
+#cp rstudio.service /etc/systemd/system/
+#systemctl enable rstudio
+#systemctl start rstudio
