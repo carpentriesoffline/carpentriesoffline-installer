4218f8c6e5bd3c0b4277706cdc1f76e363f433b4
Colin Sauze <colsau@noc.ac.uk>
2023-04-29 00:02:13 +0100

    starting jupyter and openrefine on container startup

--- docker-startup.sh
+++ docker-startup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 apache2ctl start
-su git -c "GITEA_WORK_DIR=/var/lib/gitea /usr/local/bin/gitea web --config /etc/gitea/app.ini"
-
+su git -c "GITEA_WORK_DIR=/var/lib/gitea /usr/local/bin/gitea web --config /etc/gitea/app.ini" &
+su jupyter -c "/home/jupyter/.local/bin/jupyter-lab --ip=0.0.0.0" &
+su openrefine -c "cd /home/openrefine/openrefine-3.7.2 ; ./refine -i 0.0.0.0"
