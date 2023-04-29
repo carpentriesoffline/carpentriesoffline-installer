4f44ca855faaca5c2297f6432d92d73b033be6f7
Colin Sauze <colsau@noc.ac.uk>
2023-04-29 01:15:16 +0100

    starting up rstudio

--- docker-startup.sh
+++ docker-startup.sh
@@ -3,4 +3,5 @@ apache2ctl start
 su git -c "GITEA_WORK_DIR=/var/lib/gitea /usr/local/bin/gitea web --config /etc/gitea/app.ini" &
 su jupyter -c "/home/jupyter/.local/bin/jupyter-lab --ip=0.0.0.0" &
 su etherpad -c "cd /home/etherpad/etherpad-lite/bin ; ./run.sh" &
+rstudio-server start
 su openrefine -c "cd /home/openrefine/openrefine-3.7.2 ; ./refine -i 0.0.0.0"
