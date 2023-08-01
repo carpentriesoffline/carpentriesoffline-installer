a8bbd8b4803d03026c449e10a68ce9b61f6a1c89
Colin Sauze <colsau@noc.ac.uk>
2023-08-01 19:16:05 +0100

    using a newer pip to install offlinedatasci

--- installer/setup_content.sh
+++ installer/setup_content.sh
@@ -6,6 +6,8 @@ apt install -y python3-pip r-base-core python3-lxml libssl-dev r-cran-curl
 #some weird bug means we have to try and install these twice as a download fails sometimes
 apt install -y python3-pip r-base-core python3-lxml libssl-dev r-cran-curl
 
+#ubuntu docker images provide an old version of pip which doesn't work, upgrade it
+python3 -m pip install pip -U
 pip3 install git+https://git@github.com/carpentriesoffline/offlinedatasci.git
 
 #hack to fix R version until pull request fixing this upstream is accepted
