a31b9841c2b905ed7fde17b21f4d68126fa4f844
Colin Sauze <colsau@noc.ac.uk>
2023-08-02 15:10:37 +0100

    only install offlinedatasci dependencies if we haven't got the carpentries lessons already copied

--- installer/setup_content.sh
+++ installer/setup_content.sh
@@ -1,21 +1,19 @@
 #!/bin/bash
 
-
-
-apt install -y python3-pip r-base-core python3-lxml libssl-dev r-cran-curl
-#some weird bug means we have to try and install these twice as a download fails sometimes
-apt install -y python3-pip r-base-core python3-lxml libssl-dev r-cran-curl
-
-#ubuntu docker images provide an old version of pip which doesn't work, upgrade it
-python3 -m pip install pip -U
-pip3 install git+https://git@github.com/carpentriesoffline/offlinedatasci.git
-
-#hack to fix R version until pull request fixing this upstream is accepted
-#sed -i 's/R-4.1.0/R-4.1.1/g' /usr/local/lib/python3.7/dist-packages/carpenpi/urls.py
-
-cd /var/www/html/
-
 #only run if we didn't do this already, since it takes ages and is breaking github actions builds
 if [ ! -f "/var/www/html/swcarpentry.github.io" ] ; then
+    apt install -y python3-pip r-base-core python3-lxml libssl-dev r-cran-curl
+    #some weird bug means we have to try and install these twice as a download fails sometimes
+    apt install -y python3-pip r-base-core python3-lxml libssl-dev r-cran-curl
+    
+    #ubuntu docker images provide an old version of pip which doesn't work, upgrade it
+    python3 -m pip install pip -U
+    pip3 install git+https://git@github.com/carpentriesoffline/offlinedatasci.git
+    
+    #hack to fix R version until pull request fixing this upstream is accepted
+    #sed -i 's/R-4.1.0/R-4.1.1/g' /usr/local/lib/python3.7/dist-packages/carpenpi/urls.py
+    
+    cd /var/www/html/
+
     /usr/local/bin/offlinedatasci install all .
 fi
