6f52964734c0e91864edfae76a116b7a4caf785b
Colin Sauze <colsau@noc.ac.uk>
2023-08-02 18:31:55 +0100

    switching to alternate method for getting offline datasci install

--- build_img/build_img.sh
+++ build_img/build_img.sh
@@ -78,7 +78,11 @@ echo "Updated image with password set"
 qemu-img create -f raw offlinedatasci.img 
 
 apt install -y python3-pip r-base-core python3-lxml libssl-dev r-cran-curl dosfstools 
-pip3 install offlinedatasci
+
+#ubuntu docker images provide an old version of pip which doesn't work, upgrade it
+python3 -m pip install pip -U
+pip3 install git+https://git@github.com/carpentriesoffline/offlinedatasci.git
+
 mkdir offlinedatasci
 cd offlinedatasci
 offlinedatasci install all .
