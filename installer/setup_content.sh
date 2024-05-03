ae7ec5fd53e40957df8010fc9b3d1b4fa0124155
Colin Sauze <colin.sauze@noc.ac.uk>
2024-05-03 15:03:11 +0100

    download a newer version of R on Ubuntu

--- installer/setup_content.sh
+++ installer/setup_content.sh
@@ -2,7 +2,17 @@
 
 #only run if we didn't do this already, since it takes ages and is breaking github actions builds
 if [ ! -d "/var/www/html/lessons" ] ; then
-    apt install -y python3-pip r-base-core python3-lxml libssl-dev r-cran-curl
+
+    os=`grep "^ID=" /etc/os-release | awk -F= '{print $2}'`
+    # ubuntu only comes with R 4.1, we're trying to target R 4.4, let's get a newer version
+    if [ "$os" = "ubuntu" ] ; then
+        wget -qO- https://cloud.r-project.org/bin/linux/ubuntu/marutter_pubkey.asc | tee -a /etc/apt/trusted.gpg.d/cran_ubuntu_key.asc
+        codename=`grep "^VERSION_CODENAME=" /etc/os-release | awk -F= '{print $2}'`
+        echo "deb https://cloud.r-project.org/bin/linux/ubuntu $codename-cran40/" >> /etc/apt/sources.list
+        apt -y update
+    fi
+ 
+    apt install -y python3-pip pipx r-base-core python3-lxml libssl-dev r-cran-curl
     #some weird bug means we have to try and install these twice as a download fails sometimes
     apt install -y python3-pip pipx r-base-core python3-lxml libssl-dev r-cran-curl
     
